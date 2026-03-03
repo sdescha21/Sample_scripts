bf47d7939807db04993ee7873f723dcb0cb27414
sdescha21 <sdescha21@gmail.com>
2026-03-03 13:19:09 -0500

    Create load_and_clean_data.R

---
+++ R_Scripts/load_and_clean_data.R
@@ -0,0 +1,9 @@
+# Install all required packages and load them into the environment
+install.packages("dplyr")
+library(dplyr)
+library(readr)
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("data.table")
+library(data.table)
+library(filenamer)
